9644a2cef66a7c133919fd5c2e4158bfbdd43460
BlazejUl <147735535+BlazejUl@users.noreply.github.com>
2024-11-19 19:59:22 +0100

    removed unnecesary code

--- binary.py
+++ binary.py
@@ -16,24 +16,11 @@ def binaryToBinaryList(string :str,howLong = 8)->[]:
         list.append(binary[::-1].zfill(howLong)[::-1])
     return list
 
-def binaryListToBinary(list :[],stripTrailing0 = 0)->str:
+def binaryListToBinary(list :[])->str:
     out = ""
-    isOn = 1
     for element in list:
         out += element
-        # print(out)          #debug
 
-    if stripTrailing0 == 1:
-        out = out.rstrip("0")
-        #print("out :",out)
-        h = len(out)%8
-        while isOn == 1:
-            if h != 0:
-                out += "0"
-                h += 1
-                h = len(out) % 8
-            else:
-                isOn = 0
     return out
 
 def d2StrListTod2intList(stringList :[])->[]:
